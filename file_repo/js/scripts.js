487231141afc1e9298f69fcae1f9a3e2f7c98a62
Valerii_Trebukh <Valery_Trebukh@epam.com>
2018-10-05 16:30:45 +0300

    html+css+js task

---
+++ file_repo/js/scripts.js
@@ -0,0 +1,49 @@
+function sortAsc(col) {
+	var table, rows, switching, i, x, y, shouldSwitch;
+  	table = document.getElementById("result_list_table");
+  	switching = true;
+  	while (switching) {
+	    switching = false;
+	    rows = table.rows;
+	    for (i = 1; i < (rows.length - 1); i++) {
+	    	shouldSwitch = false;
+	      	x = rows[i].getElementsByTagName("TD")[col];
+	      	y = rows[i + 1].getElementsByTagName("TD")[col];
+	      	if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+	        	shouldSwitch = true;
+	        	break;
+	      	}
+    	}
+    	if (shouldSwitch) {
+      		rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      		switching = true;
+    	}
+	}
+}
+
+function sortDesc(col) {
+	var table, rows, switching, i, x, y, shouldSwitch;
+  	table = document.getElementById("result_list_table");
+  	switching = true;
+  	while (switching) {
+	    switching = false;
+	    rows = table.rows;
+	    for (i = 1; i < (rows.length - 1); i++) {
+	    	shouldSwitch = false;
+	      	x = rows[i].getElementsByTagName("TD")[col];
+	      	y = rows[i + 1].getElementsByTagName("TD")[col];
+	      	if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+	        	shouldSwitch = true;
+	        	break;
+	      	}
+    	}
+    	if (shouldSwitch) {
+      		rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      		switching = true;
+    	}
+	}
+}
+
+function sort(col, direction) {
+	alert(col);
+}
