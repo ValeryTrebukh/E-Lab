4c8b78d3ea569e9e60dcf48b94d4a787b787e7e1
Valerii_Trebukh <Valery_Trebukh@epam.com>
2018-10-08 12:29:22 +0300

    table data sorting implemented

--- file_repo/js/scripts.js
+++ file_repo/js/scripts.js
@@ -1,49 +1,39 @@
-function sortAsc(col) {
-	var table, rows, switching, i, x, y, shouldSwitch;
-  	table = document.getElementById("result_list_table");
-  	switching = true;
-  	while (switching) {
-	    switching = false;
-	    rows = table.rows;
-	    for (i = 1; i < (rows.length - 1); i++) {
-	    	shouldSwitch = false;
-	      	x = rows[i].getElementsByTagName("TD")[col];
-	      	y = rows[i + 1].getElementsByTagName("TD")[col];
-	      	if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-	        	shouldSwitch = true;
-	        	break;
-	      	}
-    	}
-    	if (shouldSwitch) {
-      		rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      		switching = true;
-    	}
-	}
-}
-
-function sortDesc(col) {
-	var table, rows, switching, i, x, y, shouldSwitch;
-  	table = document.getElementById("result_list_table");
-  	switching = true;
-  	while (switching) {
-	    switching = false;
-	    rows = table.rows;
-	    for (i = 1; i < (rows.length - 1); i++) {
-	    	shouldSwitch = false;
-	      	x = rows[i].getElementsByTagName("TD")[col];
-	      	y = rows[i + 1].getElementsByTagName("TD")[col];
-	      	if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-	        	shouldSwitch = true;
-	        	break;
-	      	}
-    	}
-    	if (shouldSwitch) {
-      		rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      		switching = true;
-    	}
-	}
-}
-
 function sort(col, direction) {
-	alert(col);
+
+	var table, rows, switching, i, x, y, shouldSwitch;
+  	table = document.getElementById("result_list_table");
+  	switching = true;
+  	while (switching) {
+	    switching = false;
+	    rows = table.rows;
+	    for (i = 1; i < (rows.length - 1); i++) {
+	    	shouldSwitch = false;
+	      	x = rows[i].getElementsByTagName("TD")[col];
+	      	y = rows[i + 1].getElementsByTagName("TD")[col];
+			if(direction.toLowerCase() == "asc".toLowerCase()) {
+				if (compare(x.innerHTML, y.innerHTML)) {
+		        	shouldSwitch = true;
+		        	break;
+	      		}
+			} else if (direction.toLowerCase() == "desc".toLowerCase()) {
+				if (compare(y.innerHTML, x.innerHTML)) {
+		        	shouldSwitch = true;
+		        	break;
+	      		}
+			}
+    	}
+    	if (shouldSwitch) {
+      		rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      		switching = true;
+    	}
+	}
+}
+
+function compare(a, b) {
+	var res = a - b;
+	if (isNaN(res)) {
+		return a.toLowerCase() < b.toLowerCase();
+	} else {
+		return res < 0;
+	}
 }
